e11ee0c984b099ec34b586783453ff8f63afb8e0
KhoeTrinh <kelvintrinh09@gmail.com>
2024-12-02 18:56:55 +0700

    oek oek

--- src/app/app.collection.ts
+++ src/app/app.collection.ts
@@ -3,5 +3,11 @@ export class ImgSrc {
     '../../add-photo-svgrepo-com.svg',
     '../../Arrows_1_Artboard_18-1024.webp',
     '../../rectangle-thin-svgrepo-com.svg',
+    '../../download (1).jpg',
+    '../../download (2).jpg',
+    '../../OIP (1).jpg',
+    '../../OIP (2).jpg',
+    '../../OIP (3).jpg',
+    '../../th (1).jpg',
   ];
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
 import { ImgSrc } from './app.collection';
-import { NgOptimizedImage } from '@angular/common';
+import { NgOptimizedImage, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, NgStyle],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  imgSrcArray: string[] = []
+  imgSrcArray: string[] = [];
+  preImageSrcArray: number[] = [];
   constructor() {
-    const imgSources = new ImgSrc()
-    this.imgSrcArray = imgSources.imgSrc
+    const imgSources = new ImgSrc();
+    this.imgSrcArray = imgSources.imgSrc;
+    this.preImageSrcArray = this.imgSrcArray
+      .slice(2, this.preImageSrcArray.length - 1)
+      .map((_, i) => i + 3);
+    console.log(this.preImageSrcArray);
+    console.log(this.imgSrcArray);
   }
 }
