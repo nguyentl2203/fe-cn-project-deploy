2922f0255b84cb17f6dbb07c4e8d400bfe987c9d
KhoeTrinh <kelvintrinh09@gmail.com>
2024-12-04 11:03:51 +0700

    hehe

--- src/app/app.collection.ts
+++ src/app/app.collection.ts
@@ -12,6 +12,7 @@ export class ImgSrc {
     '../../search-alt-2-svgrepo-com.svg',
     '../../circle-dashed-svgrepo-com.svg'
   ];
+  emynameiscac = true
   fetchingData = {
     access_token: "CiYVu5slzDaGGAO",
     model_version: "insect_id:2.0.0",
