99cb7bfed05ee0d6fdecf0bc827568e89c50fa16
KhoeTrinh <kelvintrinh09@gmail.com>
2024-12-06 09:15:34 +0700

    moi

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { timer } from 'rxjs';
 import axios from 'axios';
 import { Collection } from './app.collection';
-import { compileNgModule } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root',
@@ -11,37 +10,31 @@ export class ApiFetchingService {
   private readonly url = 'https://be-cn-project.onrender.com/';
   constructor() {}
   async getImageInfo(data: any) {
-    const payload = {
-      images: [data]
-    }
-    const res = await axios.post(this.url, payload);
-    return res.data
+    // const payload = {
+    //   images: [data]
+    // }
+    // const res = await axios.post(this.url, payload);
+    // return res.data
     const fakeRes = new Collection();
     return new Promise<any>((resolve) => {
       timer(3000).subscribe(() => {
-        const fakeResponse = {
-          success: true,
-          message: fakeRes.fetchingData,
-        };
+        const fakeResponse = fakeRes.fetchingData
         resolve(fakeResponse);
       });
     });
   }
   async getDetailInfo(data: any) {
-    const payload ={
-      suggestion:{
-        ...data
-      }
-    };
-    const res = await axios.post(`${this.url}details`, payload);
-    return res.data;
+    // const payload ={
+    //   suggestion:{
+    //     ...data
+    //   }
+    // };
+    // const res = await axios.post(`${this.url}details`, payload);
+    // return res.data;
     const fakeRes = new Collection();
     return new Promise<any>((resolve) => {
       timer(3000).subscribe(() => {
-        const fakeGemini = {
-          success: true,
-          message: fakeRes.fetchingDataDetails,
-        };
+        const fakeGemini = fakeRes.fetchingDataDetails
         resolve(fakeGemini);
       });
     });
--- src/app/app.ts
+++ src/app/app.ts
@@ -14,14 +14,13 @@ import { FirstLetterUppercasePipe } from './app.pipe';
 export class AppComponent {
   imgSrcArray: string[] = [];
   preImageSrcArray: number[] = [];
-  uploadImgSrc: string = '';
+  uploadImgSrc: string = ' ';
   uploadImgSrcBase64: string = '';
-  hasGetImageInfo: boolean = false;
+  hasGetImageInfo: boolean = true;
   isLoading1: boolean = false;
   isLoading2: boolean = false;
   fetchingData: Array<any> = [];
-  isPopupOpen: boolean = false;
-  preI: number = -1;
+  isPopupOpen: boolean = true;
   imageInfo: any = {};
   keys: string[] = [];
   constructor(private apiService: ApiFetchingService) {
@@ -29,6 +28,7 @@ export class AppComponent {
     this.imgSrcArray = imgSources.imgSrc;
     this.preImageSrcArray = this.imgSrcArray.slice(2, 8).map((_, i) => i + 3);
     this.fetchingData = imgSources.fetchingData.result.classification.suggestions
+    this.imageInfo = imgSources.fetchingDataDetails
   }
   onFileChange(event: any): void {
     const file = event.target.files[0];
@@ -51,44 +51,13 @@ export class AppComponent {
     this.isLoading1 = false;
   }
   async getImageDetailInfo(i: number) {
-    if (this.preI === i) return;
     this.isLoading2 = true;
     const res = await this.apiService.getDetailInfo(this.fetchingData[i]);
-    const keys = Object.keys(res.data.details);
-    const outerkeys = Object.keys(res).slice(
-      1,
-      Object.keys(res).length
-    );
-    this.keys.push(
-      ...[
-        keys[0],
-        outerkeys[1],
-        outerkeys[2],
-        keys[1],
-        outerkeys[0],
-        ...keys.slice(2),
-        outerkeys[3],
-      ]
-    );
-    console.log(this.keys);
     this.imageInfo = res;
-    this.preI = i;
     this.isLoading2 = false;
   }
   objectKeys(obj: any): string[] {
     if (obj === null || typeof obj !== 'object') return [];
-    console.log(Object.keys(obj), 'object keys');
     return Object.keys(obj);
-  }
-  isArray(key: any): boolean {
-    return Array.isArray(this.imageInfo.data.details[key]);
-  }
-
-  isObject(value: any): boolean {
-    return typeof value === 'object' && value !== null && !Array.isArray(value);
-  }
-
-  isString(value: any): boolean {
-    return typeof value === 'string';
   }
 }
