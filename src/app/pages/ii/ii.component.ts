35da339e294c6a38691e246991f58879f62347db
KhoeTrinh <kelvintrinh09@gmail.com>
2024-12-09 15:31:16 +0700

    fixing card css (inprocess)

--- src/app/pages/ii/ii.component.ts
+++ src/app/pages/ii/ii.component.ts
@@ -1,5 +1,5 @@
 import { Collection } from '../../utils/app.collection';
-import { NgOptimizedImage} from '@angular/common';
+import { NgOptimizedImage } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ApiFetchingService } from '../../utils/app.service';
 import { CardComponent } from '../../components/card/card.component';
@@ -63,6 +63,7 @@ export class IIComponent {
     }
   }
   takePicture() {
+    console.log('moimoi');
     const video = this.videoElement.nativeElement;
     const canvas = this.canvasElement.nativeElement;
     const context = canvas.getContext('2d');
@@ -81,20 +82,20 @@ export class IIComponent {
     this.videoElement.nativeElement.srcObject = stream;
   }
   handleCamera(camera: { open?: boolean; is_loading?: boolean }) {
-    if (camera.is_loading) {
+    if (camera.is_loading || camera.is_loading === false) {
       this.isCameraLoading = camera.is_loading;
     }
-    if (camera.open) {
+    if (camera.open || camera.open === false) {
       this.isCameraOpen = camera.open;
     }
   }
   handleImgSrc(imgSrc: string) {
-    this.uploadImgSrc = imgSrc
+    this.uploadImgSrc = imgSrc;
   }
-  handlePopup(popup: {open: boolean, index?: number}) {
+  handlePopup(popup: { open: boolean; index?: number }) {
     this.isPopupOpen = popup.open;
-    console.log(popup.index)
-    if(popup.index || popup.index === 0) {
+    console.log(popup.index);
+    if (popup.index || popup.index === 0) {
       this.getImageDetailInfo(popup.index);
     }
   }
