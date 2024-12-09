f463b07aa6a56cc1c08c9c7c4a2c5fddb9bccc67
KhoaTrinh <kelvintrinh09@gmail.com>
2024-12-09 20:46:27 +0700

    vl:((((

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,18 @@
 import { Component } from "@angular/core";
+import { Collection } from "../../utils/app.collection";
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
     selector: 'app-about',
     standalone: true,
-    imports: [],
+    imports: [NgOptimizedImage],
     templateUrl: './about.html',
     styleUrl: './about.scss'
 })
-export class AboutComponent {}
+export class AboutComponent {
+    imgSrcArray: string[] = [];
+    constructor() {
+      const collection = new Collection();
+      this.imgSrcArray = collection.imgSrc;
+    }
+}
--- src/app/pages/donate/donate.component.ts
+++ src/app/pages/donate/donate.component.ts
@@ -1,10 +1,18 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { Collection } from '../../utils/app.collection';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
-    selector: 'app-donate',
-    standalone: true,
-    imports: [],
-    templateUrl: './donate.html',
-    styleUrl: './donate.scss'
+  selector: 'app-donate',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './donate.html',
+  styleUrl: './donate.scss',
 })
-export class DonateComponent {}
+export class DonateComponent {
+  imgSrcArray: string[] = [];
+  constructor() {
+    const collection = new Collection();
+    this.imgSrcArray = collection.imgSrc;
+  }
+}
