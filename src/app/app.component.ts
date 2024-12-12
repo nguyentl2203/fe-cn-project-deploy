9c5ccbb0b87b3fec07f779e78de7fb77a0c37eaa
KhoeTrinh <kelvintrinh09@gmail.com>
2024-12-12 10:28:21 +0700

    moi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 import { NotificationComponent } from './components/notification/notification.component';
-import { GoogleLoginProvider, SocialAuthService, SocialAuthServiceConfig, SocialLoginModule, SocialUser } from '@abacritt/angularx-social-login';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    NavbarComponent,
-    RouterOutlet,
-    NotificationComponent,
-    SocialLoginModule,
-  ],
-  providers: [
-    {
-      provide: 'SocialAuthServiceConfig',
-      useValue: {
-        autoLogin: false,
-        providers: [
-          {
-            id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider(
-              '533175683517-mb7haqqtjg5v3r6l4cjmc0cch0481q34.apps.googleusercontent.com'
-            )
-          }
-        ]
-      } as SocialAuthServiceConfig,
-    }
-  ],
+  imports: [NavbarComponent, RouterOutlet, NotificationComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrls: ['./app.scss'],
 })
-export class AppComponent {
-  user: SocialUser | null = null;
-  error: string | null = null;
+export class AppComponent implements OnInit {
+  constructor() {}
 
-  constructor(private authService: SocialAuthService) {}
+  ngOnInit(): void {
+    this.initializeGoogleSignIn();
+  }
 
-  signInWithGoogle(): void {
-    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(
-      (user) => {
-        this.user = user;
-        console.log(user);
-      },
-      (error) => {
-        this.error = error;
-      }
+  initializeGoogleSignIn() {
+    (window as any).google.accounts.id.initialize({
+      client_id: '533175683517-mb7haqqtjg5v3r6l4cjmc0cch0481q34.apps.googleusercontent.com',
+      callback: this.handleCredentialResponse.bind(this),
+    });
+
+    (window as any).google.accounts.id.renderButton(
+      document.getElementById('google-signin-button'),
+      { theme: 'outline', size: 'large' }
     );
+  }
+
+  handleCredentialResponse(response: any) {
+    console.log('Encoded JWT ID token: ' + response.credential);
   }
 }
