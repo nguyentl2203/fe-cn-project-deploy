13380ccbb2ae69c5cdc032c4659defb513467a42
KhoeTrinh <kelvintrinh09@gmail.com>
2024-12-12 08:58:29 +0700

    lmao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from "@angular/core";
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { RouterOutlet } from "@angular/router";
-import { NotificationComponent } from "./components/notification/notification.component";
+import { Component } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
+import { NotificationComponent } from './components/notification/notification.component';
+import { GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule } from '@abacritt/angularx-social-login';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [NavbarComponent, RouterOutlet, NotificationComponent],
-    templateUrl: './app.html',
-    styleUrl: './app.scss',
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    NavbarComponent,
+    RouterOutlet,
+    NotificationComponent,
+    SocialLoginModule,
+  ],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              'YOUR_GOOGLE_CLIENT_ID'
+            )
+          }
+        ]
+      } as SocialAuthServiceConfig,
+    }
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
 })
 export class AppComponent {}
