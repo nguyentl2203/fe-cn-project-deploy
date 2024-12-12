3ee490c3281730f9955705d433dd93d46f90c002
KhoeTrinh <kelvintrinh09@gmail.com>
2024-12-12 11:21:20 +0700

    moi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,4 @@ import { NotificationComponent } from './components/notification/notification.co
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
-export class AppComponent {
-  loginWithGoogle() {
-    const clientId = '533175683517-mb7haqqtjg5v3r6l4cjmc0cch0481q34.apps.googleusercontent.com';
-    const redirectUri = 'http://localhost:10000/google-auth/callback';
-    const scope = encodeURIComponent(
-      'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email'
-    );
-    const responseType = 'code';
-
-    const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`;
-
-    window.location.href = url;
-  }
-}
+export class AppComponent {}
